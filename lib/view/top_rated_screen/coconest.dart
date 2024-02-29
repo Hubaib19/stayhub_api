11f147db13dadef2e711dcf090c1eb0a489a5cd2
Hubaib19 <hubaib0019@gmail.com>
2024-02-29 14:04:22 +0530

    updated

--- lib/view/top_rated_screen/coconest.dart
+++ lib/view/top_rated_screen/coconest.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:stayhub_api/view/host_home.dart';
 
 class CoconestScreen extends StatelessWidget {
-  const CoconestScreen({super.key});
+  const CoconestScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -137,65 +137,138 @@ class CoconestScreen extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
+          const  SizedBox(height: 10,),
             Padding(
-              padding: const EdgeInsets.only(left: 40),
-              child: Row(
+              padding: const EdgeInsets.only(
+                left: 40,
+              ),
+              child: Column(
                 children: [
-                  const SizedBox(
-                    height: 1020,
-                    width: 170,
-                    child: Column(
-                      children: [
-                        Text(
-                          'Bedrooms: 3 Bathrooms: 4 Guest Capacity: 6',
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.w600),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        height: 900,
+                        width: 170,
+                        child: Column(
+                          children: [
+                            Text(
+                              'Bedrooms: 3 Bathrooms: 4 Guest Capacity: 6',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              'About:Enjoy the sounds of nature when you stay in this unique place.A shuttle will be arranged to a floating platform where the Coconest is attached. Relax on a net overhanging from the water with 360 views of the surrounding landscape including an island and King Kong mountain. Watch boats drive by as you float on the water of Tanjung Rhu in a private space dedicated for Guests looking to get away from it all, tune out and immerse themselves in nature.',
+                              style: TextStyle(
+                                  fontSize: 17, fontWeight: FontWeight.w400),
+                            )
+                          ],
                         ),
-                        Text(
-                          'About:Enjoy the sounds of nature when you stay in this unique place.A shuttle will be arranged to a floating platform where the Coconest is attached. Relax on a net overhanging from the water with 360 views of the surrounding landscape including an island and King Kong mountain. Watch boats drive by as you float on the water of Tanjung Rhu in a private space dedicated for Guests looking to get away from it all, tune out and immerse themselves in nature.',
-                          style: TextStyle(
-                              fontSize: 17, fontWeight: FontWeight.w400),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 1020,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20, right: 40),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 200),
+                        child: Container(
+                          width: 170,
+                          height: 700,
+                          decoration: BoxDecoration(
+                              color: Colors.purple[100],
+                              borderRadius: BorderRadius.circular(15)),
                           child: Column(
                             children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 20, right: 40),
+                                child: Column(
+                                  children: [
+                                    reserve(
+                                        text1: 'Check-in:',
+                                        text2: '28/02/2024'),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    reserve(
+                                        text1: 'Check-out:',
+                                        text2: '29/02/2024'),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 30),
+                                      child: Container(
+                                        width: 100,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                            color: Colors.purple,
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                           ),
+                                        child: const Center(
+                                          child: Text(
+                                            'Reserve',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                               const Text(
-                                'Check-in:',
+                                '₹5025 X ₹5025 1 nights               Tax:              ₹450 ',
                                 style: TextStyle(fontSize: 20),
                               ),
-                              Container(
-                                width: 100,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    border: Border.all(
-                                      color: Colors.grey,
-                                    )),
+                              const Text(
+                                'Total Amount:₹5475',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
                               )
                             ],
                           ),
                         ),
-                      ],
-                    ),
-                  )
+                      )
+                    ],
+                  ),
                 ],
               ),
-            )
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                helpcentre(
+                  text1: 'Support',
+                  text2: 'Help centre',
+                  text3: 'Air cover',
+                  text4: 'Air-discrimination',
+                  text5: 'Disability support',
+                  text6: 'cancellation options',
+                  text7: 'Report',
+                ),
+                helpcentre(
+                  text1: 'Hosting',
+                  text2: 'Airbnb your home',
+                  text3: 'Air cover for Hosts',
+                  text4: 'Hosting',
+                  text5: 'Resources',
+                  text6: 'Community',
+                  text7: '',
+                ),
+                helpcentre(
+                  text1: 'Airbnb',
+                  text2: 'Newsroom',
+                  text3: 'New features',
+                  text4: 'Careers',
+                  text5: 'Investors',
+                  text6: 'Airbnb.org',
+                  text7: '',
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -211,6 +284,72 @@ class CoconestScreen extends StatelessWidget {
           decoration: BoxDecoration(
               image: DecorationImage(
                   fit: BoxFit.fill, image: AssetImage(imagepath))),
+        )
+      ],
+    );
+  }
+
+  Column reserve({required String text1, required String text2}) {
+    return Column(
+      children: [
+        Text(
+          text1,
+          style: const TextStyle(fontSize: 20),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Container(
+          width: 100,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              ),
+          child: Center(
+            child: Text(
+              text2,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column helpcentre({
+    required String text1,
+    required String text2,
+    required String text3,
+    required String text4,
+    required String text5,
+    required String text6,
+    required String text7,
+  }) {
+    return Column(
+      children: [
+        Text(
+          text1,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          text2,
+          style: TextStyle(color: Colors.grey[500]),
+        ),
+        Text(text3, style: TextStyle(color: Colors.grey[500])),
+        Text(text4, style: TextStyle(color: Colors.grey[500])),
+        Text(text5, style: TextStyle(color: Colors.grey[500])),
+        Text(text6, style: TextStyle(color: Colors.grey[500])),
+        Text(text7, style: TextStyle(color: Colors.grey[500])),
+        const SizedBox(
+          height: 15,
         )
       ],
     );
